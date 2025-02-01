df0a92602fe90c7bf278404ec69832a8aba7a0e8
Tawan-ok <saiordday1@gmail.com>
2025-02-01 11:56:34 +0700

    first

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter operator (+, -, *, /): ");
+        char op = Console.ReadKey().KeyChar;
+
+        double result = op switch
+        {
+            '+' => num1 + num2,
+            '-' => num1 - num2,
+            '*' => num1 * num2,
+            '/' => num2 != 0 ? num1 / num2 : throw new DivideByZeroException(),
+            _ => throw new ArgumentException("Invalid operator")
+        };
+
+        Console.WriteLine($"\nResult: {result}");
+    }
+}
